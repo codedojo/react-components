a5898cd212e3be05e041f981ca5a93e7aae2014f
Oleg Polyakov <olegpolyakov@outlook.com>
2023-11-11 18:30:41 -0300

    Remove BreacrumbLink

--- src/components/Breadcrumb/index.ts
+++ src/components/Breadcrumb/index.ts
@@ -2,4 +2,3 @@ export { default, default as Breadcrumb, type BreadcrumbProps } from './Breadcru
 export { default as BreadcrumbButton, type BreadcrumbButtonProps } from './BreadcrumbButton';
 export { default as BreadcrumbDivider, type BreadcrumbDividerProps } from './BreadcrumbDivider';
 export { default as BreadcrumbItem, type BreadcrumbItemProps } from './BreadcrumbItem';
-export { default as BreadcrumbLink, type BreadcrumbLinkProps } from './BreadcrumbLink';
